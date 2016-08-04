9833896737316611b6dca8de471a384dc6292753
Jonathan Hall <ohsnapjon@gmail.com>
2016-08-03 19:17:46 -0700

    Add basic routes and views for testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'index#index'
-  post 'index#create'
+  post '/do-it', to: 'index#watson'
+  get '/do-it', to: 'index#watson'
 end
