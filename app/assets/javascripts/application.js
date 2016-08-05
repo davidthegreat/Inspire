8e7fbc2d7dbee3e01609c6f6da623643d885139f
Jonathan Hall <ohsnapjon@gmail.com>
2016-08-05 13:33:00 -0700

    Add pre-generated graph AJAX to page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,7 +27,8 @@ var eventListeners = function(){
 var sendForm = function(){
 	$('.quote-form').on('submit', function(event){
 	event.preventDefault();
-		var data = $(this).serialize()
+		var input = $('#submitted-quote').val();
+		var data = $(this).serialize();
 		$.ajax({
 			url: 'https://watson-api-explorer.mybluemix.net/alchemy-api/calls/html/HTMLGetTextSentiment',
 			type: 'POST',
@@ -36,8 +37,17 @@ var sendForm = function(){
 				apikey: '',
 				outputMode: 'JSON' }
 		}).done(function(response){
-			console.log(response)
+			// var photoID = Math.floor(Math.random() * 7) + 1  
+			var photoLinks = ['http://i.imgur.com/PDpXKn2.png', 'http://i.imgur.com/wQPNc3w.png', 'http://i.imgur.com/fDZmMSd.png', 'http://i.imgur.com/LW8ALXj.png', 'http://i.imgur.com/Pq9k8nv.png', 'http://i.imgur.com/K5Ayac8.png', 'http://i.imgur.com/1izp2hi.png', 'http://i.imgur.com/PDpXKn2.png' ]
+			var rand = photoLinks[Math.floor(Math.random() * photoLinks.length)];
+			$('<img class="img-responsive img-thumbnail img-circle" width="125" height="125" src="' + rand + ' "/> ').appendTo('.new-graph');
 			$('.quote-form').hide();
+			$('.new-quote').append('"' + input + '"')
+			$('.result').css("display", "inline-block")
+			// $('.new-quote').css('display: inline-block;')
+			// $('.right-arrow').css('display: inline-block;')
+			// $('.new-graph').css('display: inline-block;')
+
 		})
 	})
 } // end of sendForm
