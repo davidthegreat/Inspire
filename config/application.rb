ae3ef46135d48059e7f827493cb9a7829d67e55a
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-22 17:01:13 -0400

    Update routes and application initialization for AlchemySDK

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require_relative '../bin/alchemyapi'
+
 module RUInspiringDbchackathon
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'index#index'
-  post '/do-it', to: 'index#watson'
+  post '/alchemy', to: 'index#watson'
   get '/do-it', to: 'index#watson'
 end
