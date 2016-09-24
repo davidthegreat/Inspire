b6b706a470dd87d4f9a6e8f2d0158d2d03ca8eab
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-24 19:43:49 -0400

    Refactor watson action

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,19 +1,16 @@
 class IndexController < ApplicationController
 
-
   def index
     # home page route
   end
 
   def watson
     # route for watson form submission
+    alchemyapi = AlchemyAPI.new()
     if request.xhr?
-      input = request.raw_post
-      formatted_input = input.gsub("quote=","").gsub("%20", " ")
-      alchemyapi = AlchemyAPI.new()
-      @sentiment = alchemyapi.sentiment("text", formatted_input)
+      formatted_input = request.raw_post.gsub("quote=","").gsub("%20", " ")
       respond_to do |format|
-        format.js { render :json => @sentiment.to_json }
+        format.js { render :json => alchemyapi.sentiment("text", formatted_input).to_json }
       end
     else
       redirect_to root_path
